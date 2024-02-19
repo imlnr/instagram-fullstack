0eb6985978c44aa849e7276805a00f63ce637b71
imlnr <imlnr2002@gmail.com>
2024-02-19 16:49:01 +0530

    final commit

--- backend/routes/photo.routes.js
+++ backend/routes/photo.routes.js
@@ -3,17 +3,52 @@ const express = require('express');
 const photoRouter = express.Router();
 const multer = require('multer');
 const { PictureModel } = require('../models/photos.models');
+const { auth } = require('../middlewares/auth.middlewares');
 const upload = multer({ dest: 'uploads/' })
 
 
-photoRouter.post('/upload',async(req,res)=>{
-    const {quote,photo,device,commentsCount} = req.body;
+photoRouter.post('/upload',auth,async(req,res)=>{
+    // const {quote,photo,device,commentsCount} = req.body;
     try {
-        const picture = new PictureModel({quote,photo,device,commentsCount});
+        const picture = new PictureModel(req.body);
         await picture.save();
         res.send({"msg":"picture uploaded successfully"});
     } catch (error) {
         res.send({"msg":"get some error while uploading..."})
     }
     
+})
+
+photoRouter.patch('/uploads/:userID',auth,async(req,res)=>{
+    const {userID} = req.params;
+    try {
+        const photo = await PictureModel.findOne({_id:userID});
+        if(photo.userID == req.body.userID){
+            await PictureModel.findByIdAndUpdate({_id: userID},req.body);
+            res.send({"msg":"the image is updated successfully....."})
+        }
+        else{
+            res.status(403).send({"msg":"you are not authorized to update this photo"})
+        }
+    } catch (error) {
+        res.status(500).send({"error":error})
+    }
+})
+
+
+
+photoRouter.delete('/uploads/:userID',auth,async(req,res)=>{
+    const {userID} = req.params;
+    try {
+        const photo = await PictureModel.findOne({_id:userID});
+        if(photo.userID == req.body.userID){
+            await PictureModel.findByIdAndDelete({_id: userID});
+            res.send({"msg":"the image is deleted successfully....."})
+        }
+        else{
+            res.status(403).send({"msg":"you are not authorized to delete this photo"})
+        }
+    } catch (error) {
+        res.status(500).send({"error":error})
+    }
 })
