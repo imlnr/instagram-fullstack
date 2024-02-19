61cdca600d0dc0c91045cd7dd3be468ac7065c5b
imlnr <imlnr2002@gmail.com>
2024-02-19 14:39:35 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { connection } = require('./config/db');
 require('dotenv').config();
-
+const cors = require('cors');
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(process.env.port,async()=>{
     try {
